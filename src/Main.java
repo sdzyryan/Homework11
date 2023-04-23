443080ddce8bb74817b42e832401a082904aebf5
sdzyryan <sdzyryan@gmail.com>
2023-04-23 23:13:15 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
         task1();
         task2();
@@ -21,6 +26,23 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
+
+        int currentYear = LocalDate.now().getYear();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение соответствующее вашей ОС: 0 - iPhone, 1 - Android ");
+        int operationSystem = scanner.nextInt();
+
+        System.out.println("Введите год выпуска Вашего устройства");
+        String yearOfIssue = scanner.next();
+
+        if (operationSystem == 0) {
+            if () {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
 
     }
 }
