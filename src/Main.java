ec5a512b917c75af1f3c3a3012f63e1ecb368e93
sdzyryan <sdzyryan@gmail.com>
2023-04-24 18:10:00 +0500

    task2-3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void printLeapYear(int year) {
@@ -20,29 +21,67 @@ public class Main {
     public static void task1 () {
         System.out.println("Задача 1");
 
-        int year = 2021;
-        printLeapYear(year);
+        printLeapYear(2023);
+    }
+
+    public static void identifyOS (int typeTheOs, int yearOfIssue) {
+
+        int currentYear = LocalDate.now().getYear();
+        String mobileOs;
+        switch (typeTheOs) {
+            case 0:
+                mobileOs = "iOS";
+                break;
+            case 1:
+                mobileOs = "Android";
+                break;
+            default:
+                mobileOs = "Неизвестная ОС";
+        }
+
+        if (yearOfIssue == currentYear) {
+            System.out.println("Установите версию приложения для " + mobileOs + " по ссылке.");
+        } else {
+            System.out.println("Установите облегченную версию приложения для " + mobileOs + " по ссылке.");
+        }
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        identifyOS(1, 2023);
+        identifyOS(0, 2021);
+    }
 
-        int currentYear = LocalDate.now().getYear();
+    public static void showDeliveryDays (int deliveryDistance) {
+        int daysOfDelivery = calculationOfDeliveryDays(deliveryDistance);
+        if (daysOfDelivery > 0) {
+            System.out.println("Срок доставки: " + daysOfDelivery);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение соответствующее вашей ОС: 0 - iPhone, 1 - Android ");
-        int operationSystem = scanner.nextInt();
+    public static int calculationOfDeliveryDays (int deliveryDistance) {
+        int daysOfDelivery;
 
-        System.out.println("Введите год выпуска Вашего устройства");
-        String yearOfIssue = scanner.next();
-
-        if (operationSystem == 0) {
-            if () {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+        if (deliveryDistance > 0 && deliveryDistance <= 20) {
+            daysOfDelivery = 1;
+        } else if (deliveryDistance <= 60) {
+            daysOfDelivery = 2;
+        } else if (deliveryDistance <= 100) {
+            daysOfDelivery = 3;
+        } else {
+            daysOfDelivery = -1;
         }
 
+        return daysOfDelivery;
+    }
+
+       public static void task3 () {
+        System.out.println("Задача 3");
+        showDeliveryDays(10);
+        showDeliveryDays(50);
+        showDeliveryDays(70);
+        showDeliveryDays(200);
     }
 }
